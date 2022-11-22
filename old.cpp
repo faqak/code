1621ffcd91031d34c56285bb8ca62306f462456f
faqak <118823049+faqak@users.noreply.github.com>
2022-11-22 14:12:29 +0000

    git add -A .
    git push origin main

--- old.cpp
+++ old.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 
-using namespace std;
+#define all(x) begin(x), end(x)
+
+using i64 = long long;
 
 int main() {
-  ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+  std::ios::sync_with_stdio(false), std::cin.tie(0), std::cout.tie(0);
 
   return 0;
 }
